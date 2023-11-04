bb393317bf8fc296016c4956ac300ce9704fb3bb
Sean Yang <seanxxyang@gmail.com>
2023-11-04 14:32:35 -0400

    1.1 Daily Video 1

---
+++ src/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * Programmer: Sean Yang
+ * Date: 11/04/2023
+ * Purpose: Demonstrate the System class methods
+ */
+
+
+// Class Declaration: Identifies the name, start, and end of the class
+public class HelloWorld {
+
+    // main Method: controls all of the action in the program
+    public static void main(String[] args) {
+
+        // System.out: object that generates output to the console
+        // System.out.print: displays the arguments on screen. does not print \n char
+        System.out.print("AP ");
+        System.out.print("CS ");
+        System.out.print("A ");
+        // System.out.println: displays the arguments on screen. prints \n char
+        System.out.println("Rocks!");
+        System.out.println("Hello World");
+
+    }
+
+}
