cf19003d025ea53207069c2b37b671e67c462d6c
Sean Yang <seanxxyang@gmail.com>
2023-11-06 22:37:41 -0500

    1.1 Daily Video 2 String Literals 1.1 Daily Video 3 Syntax and Logic Errors

---
+++ src/StringLiterals.java
@@ -0,0 +1,22 @@
+/*
+ * Programmer: Sean Yang
+ * Date: 11/06/2023
+ * Purpose: Demonstrate the use of String Literals
+ */
+
+
+// A string literal in Java is an exact sequence of characters enclosed in quotation marks
+public class StringLiterals {
+	
+	public static void main(String[] args) {
+		
+		// Java will display WHATEVER is within the quotations
+		System.out.println("This is a string literal.");
+		System.out.println("ABC123#$%abc");
+		System.out.println("Have a GREAT day!!");
+		System.out.println("AP CS A Rocks!!!");
+		System.out.println("Missspelld Word");
+		
+	}
+	
+}
---
+++ src/SyntaxAndLogic.java
@@ -0,0 +1,22 @@
+/*
+ * Programmer: Sean Yang
+ * Date: 11/06/2023
+ * Purpose: Demonstrate the System class methods
+ */
+
+public class SyntaxAndLogic {
+	
+	public static void main(String[] args) {
+		
+		//Syntax error: an error in the code which will not allow the code to compile and run
+		//Logic error: an error in the code which will allow the code to run but will not produce the intended output
+		System.out.print("This ");
+		System.out.print("is a ");
+		System.out.println("test.");
+		System.out.print("AP CS ");
+		System.out.print("A Rocks!");
+		System.out.println("!!");
+		
+	}
+	
+}
